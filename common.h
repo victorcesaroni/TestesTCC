a4edb7f9e220e2003e5d06281967c84101ef515f
victorcesaroni <victor.cesaroni@gmail.com.br>
2019-05-29 15:05:10 -0300

    max filter cpu version

--- common.h
+++ common.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <cuda.h>
 #include <cuda_runtime.h>
+#include <math.h>
 
 #define DEBUG_VERBOSE_ERROR(fmt, ...) printf("[ERROR] %s:%d %s "fmt"\n", __FILE__, __LINE__, __PRETTY_FUNCTION__, ##__VA_ARGS__)
 #define DEBUG_VERBOSE_WARNING(fmt, ...) printf("[WARNING] %s:%d %s "fmt"\n", __FILE__, __LINE__, __PRETTY_FUNCTION__, ##__VA_ARGS__)
@@ -16,6 +17,9 @@
     ((ox >= 0 ? true : x >= -ox) /*x + ox >= 0*/ && x + ox < width) &&\
     ((oy >= 0 ? true : y >= -oy) /*y + oy >= 0*/ && y + oy < height) &&\
     ((oz >= 0 ? true : z >= -oz) /*z + oz >= 0*/ && z + oz < depth)
+
+
+#define DIVUP(a,b) (a+b-1)/b
 
 class CudaTimer {
   public:
@@ -69,4 +73,61 @@ class MatrixAccessor
 
     T *data;
     cudaExtent size;
+};
+
+class MemoryManager
+{
+  public:
+    template <typename T>
+    static void AllocGrayScaleImage(T **ptr, cudaExtent size)
+    {
+        size_t fileSize = size.depth * size.height * size.width * sizeof(T);
+        (*ptr) = (T *)malloc(fileSize);
+    }
+
+    static void Free(void *ptr)
+    {
+        free(ptr);
+    }
+};
+
+class FileManager
+{
+  public:
+    template <typename T>
+    static void Read(T *ptr, const char *filePath, cudaExtent size)
+    {
+        size_t fileSize = size.depth * size.height * size.width * sizeof(T);
+
+        FILE *file = fopen(filePath, "r");
+        size_t read = fread(ptr, fileSize, 1, file);        
+        fclose(file);
+    }
+
+    template <typename original_t, typename as_t>
+    static void ReadAs(as_t *ptr, const char *filePath, cudaExtent size)
+    {
+        size_t fileSize = size.depth * size.height * size.width * sizeof(original_t);
+        unsigned char *tmp = (unsigned char*)malloc(fileSize);
+
+        FILE *file = fopen(filePath, "r");
+        size_t read = fread(tmp, fileSize, 1, file);
+        fclose(file);
+
+        size_t j = 0;
+        for (size_t i = 0; i < fileSize; i += sizeof(original_t))
+            ptr[j++] = (as_t)(*(original_t*)&tmp[i]);        
+
+        free(tmp);
+    }
+
+    template <typename T>
+    static void Write(T *ptr, const char *filePath, cudaExtent size)
+    {
+        size_t fileSize = size.depth * size.height * size.width * sizeof(T);
+
+        FILE *file = fopen(filePath, "w+");
+        size_t write = fwrite(ptr, fileSize, 1, file);
+        fclose(file);
+    }
 };
--- main.cu
+++ main.cu
@@ -4,48 +4,6 @@
 
 #include "common.h"
 
-class MemoryManager
-{
-  public:
-    template <typename T>
-    static void AllocGrayScaleImage(T **ptr, cudaExtent size)
-    {
-        size_t fileSize = size.depth * size.height * size.width * sizeof(T);
-        (*ptr) = (T *)malloc(fileSize);
-    }
-
-    static void Free(void *ptr)
-    {
-        free(ptr);
-    }
-};
-
-class FileManager
-{
-  public:
-    template <typename T>
-    static void Read(T **ptr, const char *filePath, cudaExtent size)
-    {
-        FILE *file = fopen(filePath, "r");
-        size_t fileSize = size.depth * size.height * size.width * sizeof(T);
-
-        size_t read = fread((*ptr), fileSize, 1, file);
-        
-        fclose(file);
-    }
-
-    template <typename T>
-    static void Write(T *ptr, const char *filePath, cudaExtent size)
-    {
-        FILE *file = fopen(filePath, "w+");
-        size_t fileSize = size.depth * size.height * size.width * sizeof(T);
-
-        size_t write = fwrite(ptr, fileSize, 1, file);
-
-        fclose(file);
-    }
-};
-
 template <typename T>
 class InputGenerator
 {
@@ -114,26 +72,89 @@ void MaxFilterCPU(val_t *pInput, val_t *pOutput, cudaExtent size, MaxFilterParam
     }
 }
 
+template <typename val_t>
+void MaxFilterCPUParallel(val_t *pInput, val_t *pOutput, cudaExtent size, MaxFilterParams params)
+{
+    MatrixAccessor<val_t> input(pInput, size);
+    MatrixAccessor<val_t> output(pOutput, size);
+
+    auto kernel = [&](dim3 blockIdx, dim3 blockDim, dim3 threadIdx)
+    {
+        size_t x = blockIdx.x * blockDim.x + threadIdx.x;
+        size_t y = blockIdx.y * blockDim.y + threadIdx.y;
+        size_t z = blockIdx.z * blockDim.z + threadIdx.z;
+
+        if (x < size.width && y < size.height && z < size.depth)
+        {
+            val_t maxVal = input.Get(x, y, z);
+
+            for (int oy = -params.radius; oy <= params.radius; oy++)
+            {
+                for (int ox = -params.radius; ox <= params.radius; ox++)
+                {
+                    if (input.CheckOffsetInBounds(x, y, z, ox, oy, 0))
+                        maxVal = max(maxVal, input.Get(x+ox, y+oy, z));
+                }
+            }
+
+            output.Get(x, y, z) = maxVal;
+        }
+    };
+
+    const size_t BLOCK_SIZE = 512;
+    const size_t NUM_THREADS = 10;
+
+    dim3 blocks(DIVUP(size.width, BLOCK_SIZE), DIVUP(size.height, BLOCK_SIZE), DIVUP(size.depth, BLOCK_SIZE));
+
+    for (size_t bz = 0; bz < blocks.z; bz++)
+    for (size_t by = 0; by < blocks.y; by++)
+    for (size_t bx = 0; bx < blocks.x; bx++)
+    {
+        dim3 blockIdx(bx, by, bz);
+
+        for (size_t tz = 0; tz < BLOCK_SIZE; tz++)
+        for (size_t ty = 0; ty < BLOCK_SIZE; ty++)
+        for (size_t tx = 0; tx < BLOCK_SIZE; tx++)
+        {
+            dim3 threadIdx(tx, ty, tz);
+            kernel(blockIdx, dim3(BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE), threadIdx);
+        }
+    }
+}
+
 int main(int argc, const char *argv[])
 {
-    //nvcc main.cu -Xcompiler -fopenmp --compiler-options -fPIC -O3 -D__INTEL_COMPILER -o ../bin/3d
+    //nvcc main.cu --std=c++11 -Xcompiler -fopenmp --compiler-options -fPIC -O3 -D__INTEL_COMPILER -o main
 
-    cudaExtent size = make_cudaExtent(10, 10, 1);
+    InputGenerator<float>::Generate("test.b", make_cudaExtent(10, 10, 10));
 
-    InputGenerator<float>::Generate("test.b", size);
+    const char *inputPath = argv[1];
+    const char *outputPath = argv[2];
+    size_t width = atoi(argv[3]);
+    size_t height = atoi(argv[4]);
+    size_t depth = atoi(argv[5]);
+
+    cudaExtent size = make_cudaExtent(width, height, depth);
 
     float *input = NULL;
     float *output = NULL;
     MemoryManager::AllocGrayScaleImage<float>(&input, size);
     MemoryManager::AllocGrayScaleImage<float>(&output, size);
 
-    FileManager::Read<float>(&input, "test.b", size);
+    FileManager::ReadAs<unsigned short, float>(input, inputPath, size);
 
+    CudaTimer timer;
+    printf("MaxFilterCPU\n");
     MaxFilterParams params;
-    params.radius = 2;
-    MaxFilterCPU(input, output, size, params);
+    params.radius = 8;
 
-    FileManager::Write<float>(output, "test2.b", size);
+    timer.start();
+    MaxFilterCPUParallel<float>(input, output, size, params);
+    timer.stop();
+
+    printf("MaxFilterCPU %fms\n", timer.elapsedMs);
+
+    FileManager::Write<float>(output, outputPath, size);
 
     MemoryManager::Free(input);
     MemoryManager::Free(output);
