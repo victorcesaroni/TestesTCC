684542ddab5a2572bd921e325affff960799c051
victorcesaroni <victor.cesaroni@gmail.com>
2019-06-03 22:26:17 -0300

    cmake project

--- src/common.h
+++ src/common.h
@@ -54,19 +54,19 @@ class MatrixAccessor
 {
   public:
     // Row major matrix acessor
-    MatrixAccessor(T *data, cudaExtent size)
+    __host__ __device__ MatrixAccessor(T *data, cudaExtent size)
         : data(data), size(size)
     {        
     }
 
-    __forceinline__ T& Get(size_t x, size_t y, size_t z)
+    __host__ __device__ __forceinline__ T& Get(size_t x, size_t y, size_t z)
     {
 #ifdef __CUDA_ARCH__
         // device code (gpu)
-        return data[z*size.depth*y*size.height + y*size.width + x];
+        return data[z*size.height*size.width + y*size.width + x];
 #else
         // host code (cpu)
-        return data[z*size.depth*y*size.height + y*size.width + x];
+        return data[z*size.height*size.width + y*size.width + x];
 #endif
     }
 
--- src/main.cu
+++ src/main.cu
@@ -66,6 +66,97 @@ void MaxFilterCPU(val_t *pInput, val_t *pOutput, cudaExtent size, MaxFilterParam
     }
 }
 
+
+template <typename val_t>
+__global__ void CopyKernel(val_t *d_input, val_t *d_output, cudaExtent size)
+{
+    size_t x = blockIdx.x * blockDim.x + threadIdx.x;
+    size_t y = blockIdx.y * blockDim.y + threadIdx.y;
+    size_t z = blockIdx.z * blockDim.z + threadIdx.z;
+
+    MatrixAccessor<val_t> input(d_input, size);
+    MatrixAccessor<val_t> output(d_output, size);
+    
+    if (x < size.width && y < size.height && z < size.depth)
+    {
+        output.Get(x, y, z) = input.Get(x, y, z);
+    }
+}
+
+void TestCUDA()
+{
+    printf("TEST CUDA\n");
+
+    cudaExtent size = make_cudaExtent(4096, 1023, 17);
+
+    size_t bytes = size.depth * size.height * size.width * sizeof(float);
+    
+    printf("Alloc CPU\n");
+    float *pInput = (float*)malloc(bytes);
+    float *pOutput = (float*)malloc(bytes);
+
+    float *d_input;
+    float *d_output;
+    cudaError_t e;    
+
+    printf("Alloc GPU\n");
+    e = cudaMalloc(&d_input, bytes);
+    CHECK_CUDA_ERROR(e);
+    e = cudaMalloc(&d_output, bytes);
+    CHECK_CUDA_ERROR(e);
+
+    MatrixAccessor<float> input(pInput, size);
+    MatrixAccessor<float> output(pOutput, size);
+
+    for (size_t z = 0; z < size.depth; z++)
+    for (size_t y = 0; y < size.height; y++)
+    for (size_t x = 0; x < size.width; x++)
+    {
+        input.Get(x,y,z) = (float)(rand() % 255);
+    }
+
+    printf("H2D\n");
+    e = cudaMemcpy(d_input, pInput, bytes, cudaMemcpyHostToDevice);
+    CHECK_CUDA_ERROR(e);
+    
+    printf("KERNEL\n");
+    const size_t BLOCK_SIZE = 8;
+    dim3 blocks(DIVUP(size.width, BLOCK_SIZE), DIVUP(size.height, BLOCK_SIZE), DIVUP(size.depth, BLOCK_SIZE));
+    dim3 threads(BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+
+    CopyKernel<<<blocks,threads>>>(d_input, d_output, size);
+    cudaDeviceSynchronize();
+
+    printf("D2H\n");
+    e = cudaMemcpy(pOutput, d_output, bytes, cudaMemcpyDeviceToHost);
+    CHECK_CUDA_ERROR(e);
+
+    for (size_t z = 0; z < size.depth; z++)
+    for (size_t y = 0; y < size.height; y++)
+    {
+        for (size_t x = 0; x < size.width; x++)
+        {
+            float a = input.Get(x,y,z);
+            float b = output.Get(x,y,z);
+
+            //printf("%f ", b);
+            if (fabsf(fabsf(a) - fabsf(b)) > 0.01f)
+            {
+                printf("TEST CUDA ERROR %f %f\n", a, b);
+            }
+        }
+        //printf("\n");
+    }
+
+    printf("TEST END\n");
+
+    free(pInput);
+    free(pOutput);
+
+    cudaFree(d_input);
+    cudaFree(d_output);
+}
+
 template <typename val_t>
 void MaxFilterParallel(val_t *pInput, val_t *pOutput, cudaExtent size, MaxFilterParams params, bool gpu)
 {
@@ -133,8 +224,11 @@ void MaxFilterParallel(val_t *pInput, val_t *pOutput, cudaExtent size, MaxFilter
 int main(int argc, const char *argv[])
 {
     //nvcc main.cu --expt-extended-lambda --std=c++11 -Xcompiler -fopenmp --compiler-options -fPIC -O3 -D__INTEL_COMPILER -o main
+    //nvcc main.cu -gencode arch=compute_61,code=[sm_61,compute_61] --expt-extended-lambda --std=c++11 -Xcompiler -fopenmp --compiler-options -fPIC -O3 -D__INTEL_COMPILER -DCMAKE_C_COMPILER=/usr/bin/gcc-6 -o main
+    
+    TestCUDA();
 
-    InputGenerator<float>::Generate("test.b", make_cudaExtent(10, 10, 10));
+    InputGenerator<float>::Generate("test.b", make_cudaExtent(10, 10, 1));
 
     const char *inputPath = argv[1];
     const char *outputPath = argv[2];
